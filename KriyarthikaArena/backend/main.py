e7d442873b511165aa3519aabbab035a23224754
Mahika Bagri <arpitabagri@Arpitas-MacBook-Pro.local>
2026-03-19 11:56:27 -0700

    debugging

--- KriyarthikaArena/backend/main.py
+++ KriyarthikaArena/backend/main.py
@@ -17,9 +17,7 @@ def startup_migrations():
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "https://kriyarthika-arena-kappa.vercel.app",
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
